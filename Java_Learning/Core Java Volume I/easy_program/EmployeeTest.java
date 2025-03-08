f0629e28ffd6adafd18cac460a500b91c5d42fe1
lyy42995004 <3519480004@qq.com>
2025-03-08 17:58:09 +0800

    java code

--- Java_Learning/Core Java Volume I/easy_program/EmployeeTest.java
+++ Java_Learning/Core Java Volume I/easy_program/EmployeeTest.java
@@ -1,13 +1,15 @@
 import java.time.*;
+import java.util.*;
 
 public class EmployeeTest {
     public static void main() {
         // fill the staff array with three Employee objects
-        Employee[] staff = new Employee[3];
+        // Employee[] staff = new Employee[3];
+        var staff = new ArrayList<Employee>();
 
-        staff[0] = new Employee("Carl Cracker", 75000, 1987, 12, 15);
-        staff[1] = new Employee("Harry Hacker", 50000, 1989, 10, 1);
-        staff[2] = new Employee("Tony Tester", 40000, 1990, 3, 15);
+        staff.add(new Employee("Carl Cracker", 75000, 1987, 12, 15));
+        staff.add(new Employee("Harry Hacker", 50000, 1989, 10, 1));
+        staff.add(new Employee("Tony Tester", 40000, 1990, 3, 15));
 
         // raise everyone's salary by 5%
         for (Employee e : staff)
