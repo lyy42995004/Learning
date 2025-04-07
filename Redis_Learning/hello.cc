238d14f5f5cb01084639025789155715aa04d651
lyy42995004 <3519480004@qq.com>
2025-04-07 21:22:28 +0800

    go

--- Redis_Learning/hello.cc
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include <sw/redis++/redis++.h>
-
-int main() {
-    sw::redis::Redis redis("tcp://127.0.0.1:6379");
-    std::string result = redis.ping();
-    std::cout << result << std::endl;
-    return 0;
-}
