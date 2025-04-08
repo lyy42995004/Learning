1ef9d18934caa98e685651a11207a1ac4123c4da
lyy42995004 <3519480004@qq.com>
2025-04-08 21:18:03 +0800

    go

---
+++ Go_Learning/Go Programming Language/ch1/echo/main/echo.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+// echo3
+func main() {
+	fmt.Println(strings.Join(os.Args[1:], " "));
+}
+
+// echo2
+// func main() {
+// 	s, sep := "", ""
+// 	for _, arg := range os.Args[1:] {
+// 		s += sep + arg;
+// 		sep = " ";
+// 	}
+// 	fmt.Println(s);
+// }
+
+// echo1
+// func main() {
+// 	var s, sep string
+// 	for i := 1; i < len(os.Args); i++ {
+// 		s += sep + os.Args[i];
+// 		sep = " ";
+// 	}
+// 	fmt.Println(s);
+// }
